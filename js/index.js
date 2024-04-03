4760e8b0ffb890356b953bbd93814f53de086e3d
VictorIgle <165699685+VictorIgle@users.noreply.github.com>
2024-04-03 16:32:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,16 +6,13 @@ Cuando hago MouseLeave en h2 REMOVE isVisible*/
 const names = document.querySelectorAll('.Main-h')
 const images = document.querySelectorAll('.Wrapper-img')
 
-console.log(images)
-console.log(names)
-
-names.forEach(function( eachName , i){
-    names[i].addEventListener(`mouseenter` , function(e){
+names.forEach(( eachName , i)=>{
+    names[i].addEventListener(`mouseenter` , (e)=>
         images[i].classList.add('isVisible')
         
-    })
-    names[i].addEventListener(`mouseleave` , function(e){
+    )
+    names[i].addEventListener(`mouseleave` , (e)=>
         images[i].classList.remove('isVisible')
         
-    })
+    )
 })
