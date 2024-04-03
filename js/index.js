01ba2019653a22fbd4409765669a76bc89fcb990
VictorIgle <165699685+VictorIgle@users.noreply.github.com>
2024-04-03 17:01:14 +0200

    Js. Change
    cambio de etiqueta en js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 Cuando hago MouseLeave en h2 REMOVE isVisible*/
 
 const names = document.querySelectorAll('.Main-h')
-const images = document.querySelectorAll('.Wrapper-img')
+const images = document.querySelectorAll('.Main-img')
 
 names.forEach(( eachName , i)=>{
     names[i].addEventListener(`mouseenter` , (e)=>
